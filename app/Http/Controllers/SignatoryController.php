d49feadb54d02c7edaad4c046f9345d09fb7be34
William Tomé <william.tomenh@gmail.com>
2020-04-10 19:46:16 -0300

    Controllers defined.

---
+++ app/Http/Controllers/SignatoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Signatory;
+use Illuminate\Http\Request;
+
+class SignatoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Signatory  $signatory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Signatory $signatory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Signatory  $signatory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Signatory $signatory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Signatory  $signatory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Signatory $signatory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Signatory  $signatory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Signatory $signatory)
+    {
+        //
+    }
+}
