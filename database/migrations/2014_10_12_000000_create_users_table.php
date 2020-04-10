a10fde00bf799d0bbda3d9d3ab9c194fac45c9bd
William Tomé <william.tomenh@gmail.com>
2020-04-10 19:45:08 -0300

    CPF and signature fields defined.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('cpf', 11);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('signature', 45);
             $table->rememberToken();
             $table->timestamps();
         });
