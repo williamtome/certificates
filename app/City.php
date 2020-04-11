ff2a8bbfcfb2798fb783d2a10eb87f1308822f86
William Tomé <william.tomenh@gmail.com>
2020-04-11 18:36:06 -0300

    Add relationship between cities and states table. Add fillable at columns.

--- app/City.php
+++ app/City.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     public $table = 'cities';
+
+    public $fillable = ['name', 'state_id'];
+
+    public $timestamps = false;
+
+    public function state(){
+        return $this->belongsTo(State::class);
+    }
 }
