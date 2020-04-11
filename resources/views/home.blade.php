60b4fa43a1c68236a71ec7a6c47970a176c0e306
William Tomé <william.tomenh@gmail.com>
2020-04-11 18:38:27 -0300

    Menu created.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,11 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <ul>
+                        <li><a href="{{ route('city.index') }}">Cadastro de Cidades</a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
