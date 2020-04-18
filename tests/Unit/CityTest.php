b7147716bf6b31b596b9d1db33af5b8d074f9fa7
William Tomé <william.tomenh@gmail.com>
2020-04-18 09:33:56 -0300

    Test City model.

---
+++ tests/Unit/CityTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CityTest extends TestCase{
+
+    public function testIfExistsAttribute()
+    {
+        $this->assertClassHasAttribute('table', App\City::class);
+    }
+
+    public function FunctionName(Type $var = null)
+    {
+        # code...
+    }
+
+}
