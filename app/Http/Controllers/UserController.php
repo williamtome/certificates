b38581c45d7d37ee6c504cf4ce42f5f5ac48ca54
William Tomé <william.tomenh@gmail.com>
2020-04-18 16:46:55 -0300

    User controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('user.index', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        return view('user.create', ['user' => new User()]);
+    }
+
+    public function store(UserRequest $request)
+    {
+        dd($request->store('signature'));
+        User::create([
+            'cpf' => $request->cpf,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'signature' => $request->signature
+        ]);
+
+        return redirect()->route('user.index');
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
